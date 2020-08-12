299c89e3d1f34794d267193173be63a13efaa3fd
Guilherme Mota <guilhermemota08@hotmail.com>
2020-08-12 20:34:53 -0300

    rest

---
+++ src/app/restaurants/restaurant-info/restaurant-info.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-info',
+  templateUrl: './restaurant-info.page.html',
+  styleUrls: ['./restaurant-info.page.scss'],
+})
+export class RestaurantInfoPage implements OnInit {
+
+  status = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
